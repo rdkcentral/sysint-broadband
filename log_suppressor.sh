74f283eb80cfc9395b114542d417154116c28e5b
Nithishkumar-T <109725053+Nithishkumar-T@users.noreply.github.com>
2026-02-18 11:10:42 +0530

    Update log_suppressor.sh

--- log_suppressor.sh
+++ log_suppressor.sh
@@ -15,144 +15,116 @@ for input_file in "$input_dir"/*; do
     output_file="$output_dir/$filename"
 
     awk '
-    BEGIN { OFS = " : " }
+    BEGIN {
+        prev_message = ""
+        repeat_count = 0
+        first_line = ""
+        timestamp_list = ""
+    }
+
+    function flush_repeated() {
+        if (repeat_count > 0) {
+            if (repeat_count > 1) {
+                print first_line " (Suppressed count: " (repeat_count - 1) ", Occurrences: " timestamp_list ".)"
+            } else {
+                print first_line
+            }
+            repeat_count = 0
+            prev_message = ""
+            first_line = ""
+            timestamp_list = ""
+        }
+    }
 
     {
         if (length($0) == 0) next
 
+        has_timestamp = 0
+        timestamp = ""
+        message = ""
+
+        # Extract timestamp and message using various formats
         if (match($0, /^[0-9]{6}-[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6} /)) {
             timestamp = substr($0, 1, RLENGTH)
             message = substr($0, RLENGTH + 1)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^[0-9-]+-[0-9:.]+ /)) {
             timestamp = substr($0, 1, RLENGTH)
             message = substr($0, RLENGTH + 1)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^[0-9]{4} [A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} /)) {
             timestamp = substr($0, 1, RLENGTH)
             message = substr($0, RLENGTH + 1)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^\[[A-Za-z]{3} [A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}\] /)) {
             timestamp = substr($0, 2, RLENGTH - 2)
             message = substr($0, RLENGTH + 2)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^[A-Za-z]+, [A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}:/)) {
             timestamp = substr($0, 1, RLENGTH - 1)
             message = substr($0, RLENGTH + 1)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^[0-9]{8} [0-9]{6}\.[0-9]{6} /)) {
             timestamp = substr($0, 1, RLENGTH)
             message = substr($0, RLENGTH + 1)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^[A-Za-z]{3} [A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [A-Za-z]{3} [0-9]{4} /) || match($0, /^[A-Za-z]{3} [A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4} /)) {
             timestamp = substr($0, 1, RLENGTH)
             message = substr($0, RLENGTH + 1)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^\[OneWifi\] [0-9]{6}-[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}<[IE]> /)) {
             timestamp = substr($0, 11, 17)
             message = substr($0, 30)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^[0-9]{4}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} /)) {
             timestamp = substr($0, 1, 19)
             message = substr($0, 21)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^\[([0-9]{2}:[0-9]{2}:[0-9]{2}) ([0-9]{2}\/[0-9]{2}\/[0-9]{4})\] /)) {
             timestamp = substr($0, 2, RLENGTH - 3)
             message = substr($0, RLENGTH + 2)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6} /)) {
             timestamp = substr($0, 1, RLENGTH)
             message = substr($0, RLENGTH + 1)
-            key = message
             has_timestamp = 1
         } else if (match($0, /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9:.]+Z[[:space:]]*:/)) {
             timestamp = substr($0, 1, RLENGTH)
             message = substr($0, RLENGTH + 1)
-            key = message
             has_timestamp = 1
-        } else {
-            # Line has no recognizable timestamp → flush buffer and print raw line
-
-            if (num_messages > 0) {
-                for (i = 1; i <= num_messages; i++) {
-                key = order[i]
-                split(messages[key], data, "\t")
-                count = data[1]
-                first_line = data[2]
-                timestamps = data[3]
-
-                if (count > 1)
-                    print first_line " (Suppressed count: " count ", Occurrences: " timestamps ".)"
-                else
-                    print first_line
-                }
-
-            delete messages
-            delete order
-            num_messages = 0
-            }
-
-        print $0
-        next
         }
 
-
+        # Handle lines with timestamps
         if (has_timestamp) {
-            if (key in messages) {
-                split(messages[key], data, "\t")
-                count = data[1] + 1
-                first_line = data[2]
-                timestamps = data[3] (data[3] != "" ? ", " : "") timestamp
-                messages[key] = count "\t" first_line "\t" timestamps
+            # Check if this message is the same as previous (consecutive repetition)
+            if (message == prev_message) {
+                # Consecutive repetition detected
+                repeat_count++
+                if (timestamp_list != "") {
+                    timestamp_list = timestamp_list ", " timestamp
+                } else {
+                    timestamp_list = timestamp
+                }
             } else {
-                messages[key] = "1\t" $0 "\t" timestamp
-                order[++num_messages] = key
+                # Different message - flush previous if any
+                flush_repeated()
+                
+                # Start tracking this new message
+                prev_message = message
+                repeat_count = 1
+                first_line = $0
+                timestamp_list = timestamp
             }
         } else {
-            if (num_messages > 0) {
-                for (i = 1; i <= num_messages; i++) {
-                    key = order[i]
-                    split(messages[key], data, "\t")
-                    count = data[1]
-                    first_line = data[2]
-                    timestamps = data[3]
-                    if (count > 1) {
-                        print first_line " (Suppressed count: " count ", Occurrences: " timestamps ".)"
-                    } else {
-                        print first_line
-                    }
-                }
-                delete messages
-                delete order
-                num_messages = 0
-            }
-            print message
+            # Line without timestamp - flush any pending and print as-is
+            flush_repeated()
+            print $0
         }
     }
 
     END {
-        for (i = 1; i <= num_messages; i++) {
-            key = order[i]
-            split(messages[key], data, "\t")
-            count = data[1]
-            first_line = data[2]
-            timestamps = data[3]
-            if (count > 1) {
-                print first_line " (Suppressed count: " count ", Occurrences: " timestamps ".)"
-            } else {
-                print first_line
-            }
-        }
+        # Flush any remaining repeated messages
+        flush_repeated()
     }' "$input_file" > "$output_file"
 
 done
