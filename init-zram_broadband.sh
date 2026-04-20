b022cf5ed5704b49de6b596f9cf5c861fe53967f
Dane Murphy <danem7@gmail.com>
2026-04-20 11:41:04 -0700

    Add empty checks for MEMSWAP configuration returns from the data model

--- init-zram_broadband.sh
+++ init-zram_broadband.sh
@@ -41,28 +41,54 @@ if [ "x$MEMSWAP_RFC_ENABLE" != "xtrue" ]; then
 fi
 
 # Load ZRAM module with one block device for SWAP
-modprobe zram num_devices=1
+if modinfo zram | grep -q ' zram_num_devices:' 2>/dev/null; then
+    MODPROBE_ARGS="zram_num_devices=${NRDEVICES}"
+elif modinfo zram | grep -q ' num_devices:' 2>/dev/null; then
+    MODPROBE_ARGS="num_devices=${NRDEVICES}"
+else
+    exit 1
+fi
+modprobe zram $MODPROBE_ARGS
+
+# Wait for the module to load
+sleep 3
 
 # Configure the disk size
 MEMSWAP_DISK_SIZE=$(dmcli eRT retv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.MEMSWAP.DiskSize)
+if [ -z "$MEMSWAP_DISK_SIZE" ]; then
+    echo_t "MEMSWAP disk size not retrieved from RFC"
+    exit 1
+fi
 echo "${MEMSWAP_DISK_SIZE}M" >/sys/block/zram0/disksize
 echo_t "MEMSWAP disk size set to ${MEMSWAP_DISK_SIZE}M"
 
 # Configure the system swappiness
 MEMSWAP_TUNABLES_SWAPPINESS=$(dmcli eRT retv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.MEMSWAP.Tunables.Swappiness)
+if [ -z "$MEMSWAP_TUNABLES_SWAPPINESS" ]; then
+    echo_t "MEMSWAP swappiness not retrieved from RFC"
+    exit 1
+fi
 sysctl -w vm.swappiness="$MEMSWAP_TUNABLES_SWAPPINESS"
 echo_t "System swappiness set to ${MEMSWAP_TUNABLES_SWAPPINESS}"
 
 # Configure the system watermark scale factor
 MEMSWAP_TUNABLES_WATERMARK_SCALE_FACTOR=$(dmcli eRT retv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.MEMSWAP.Tunables.WatermarkScaleFactor)
+if [ -z "$MEMSWAP_TUNABLES_WATERMARK_SCALE_FACTOR" ]; then
+    echo_t "MEMSWAP watermark scale factor not retrieved from RFC"
+    exit 1
+fi
 sysctl -w vm.watermark_scale_factor="$MEMSWAP_TUNABLES_WATERMARK_SCALE_FACTOR"
 echo_t "System watermark scale factor set to ${MEMSWAP_TUNABLES_WATERMARK_SCALE_FACTOR}"
 
 # Configure the system page cluster for SWAP
 MEMSWAP_TUNABLES_PAGE_CLUSTER=$(dmcli eRT retv Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.MEMSWAP.Tunables.PageCluster)
+if [ -z "$MEMSWAP_TUNABLES_PAGE_CLUSTER" ]; then
+    echo_t "MEMSWAP page cluster not retrieved from RFC"
+    exit 1
+fi
 sysctl -w vm.page-cluster="$MEMSWAP_TUNABLES_PAGE_CLUSTER"
 echo_t "System page cluster for SWAP set to ${MEMSWAP_TUNABLES_PAGE_CLUSTER}"
 
 # Enable the ZRAM SWAP device
 mkswap /dev/zram0
-swapon /dev/zram0
+swapon -p 5 /dev/zram0
